d6c5c8df971f6dfb4f8aa973dccf2c27aef45dd3
Marcelo de Oliveira Santos <=>
2023-09-15 09:25:26 -0300

    Page Obgect pattern applied to Calculadoratest

--- src/main/java/br/dev/marcelodeoliveira/appium/tests/CalculadoraTest.java
+++ src/main/java/br/dev/marcelodeoliveira/appium/tests/CalculadoraTest.java
@@ -11,44 +11,26 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.dev.marcelodeoliveira.appium.tests.model.pages.CalculadoraPage;
 import io.appium.java_client.MobileElement;
 
 public class CalculadoraTest extends BaseTest {
+
+	CalculadoraPage page;
+
 	@Before
 	public void setupTest() {
 		setDefaultCapabilities();
 		addCapability("appPackage", "com.google.android.calculator");
 		addCapability("appActivity", "com.android.calculator2.Calculator");
 		setupDriver();
+		this.page = new CalculadoraPage();
 	}
 
 	@Test
 	public void calculadoraSomaTest() throws MalformedURLException {
-
-		MobileElement el2 = (MobileElement) getDriver().findElementById("com.google.android.calculator:id/digit_2");
-		el2.click();
-
-		MobileElement el3 = (MobileElement) getDriver().findElementById("com.google.android.calculator:id/op_add");
-
-		el3.click();
-		MobileElement el4 = (MobileElement) getDriver().findElementById("com.google.android.calculator:id/digit_2");
-
-		el4.click();
-		MobileElement el5 = (MobileElement) getDriver().findElementById("com.google.android.calculator:id/eq");
-
-		el5.click();
-		MobileElement el6 = (MobileElement) getDriver()
-				.findElementById("com.google.android.calculator:id/result_final");
-
-		// it fails: their references don't point to the same object;
-		// Assert.assertSame(el2, el4);
-		Assert.assertEquals(el2, el4);
-		Assert.assertEquals(el2.getAttribute("content-desc"), "2", el2.getAttribute("content-desc"));
-		Assert.assertEquals(el3.getAttribute("content-desc"), "mais", el3.getAttribute("content-desc"));
-		Assert.assertEquals(el4.getAttribute("content-desc"), "2", el4.getAttribute("content-desc"));
-		Assert.assertEquals(el5.getAttribute("content-desc"), "igual", el5.getAttribute("content-desc"));
-		Assert.assertEquals(el6.getAttribute("content-desc"), "4", el6.getText());
-
+		page.simpleSum(2, 2);
+		Assert.assertEquals(page.getResult(),"4");
 	}
 
 }
--- src/main/java/br/dev/marcelodeoliveira/appium/tests/model/pages/CalculadoraPage.java
+++ src/main/java/br/dev/marcelodeoliveira/appium/tests/model/pages/CalculadoraPage.java
@@ -1,5 +1,125 @@
 package br.dev.marcelodeoliveira.appium.tests.model.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
+import br.dev.marcelodeoliveira.appium.core.DriverFactory;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
 public class CalculadoraPage extends BasePage {
-	
+
+	public CalculadoraPage() {
+		PageFactory.initElements(new AppiumFieldDecorator(DriverFactory.getDriver()), this);
+	}
+
+	@AndroidFindBy(id = "com.google.android.calculator:id/op_add")
+	private MobileElement btnAdd;
+
+	@AndroidFindBy(id = "com.google.android.calculator:id/eq")
+	private MobileElement btnEquals;
+
+	@AndroidFindBy(id = "com.google.android.calculator:id/result_final")
+	private MobileElement lblResult;
+
+	@AndroidFindBy(id = "com.google.android.calculator:id/op_sub")
+	private MobileElement btnMinus;
+
+	@AndroidFindBy(id = "com.google.android.calculator:id/op_mul")
+	private MobileElement btnTimes;
+	@AndroidFindBy(id = "com.google.android.calculator:id/op_div")
+	private MobileElement btnDividedTo;
+	@AndroidFindBy(id = "com.google.android.calculator:id/op_pct")
+	private MobileElement btnPercent;
+	@AndroidFindBy(id = "com.google.android.calculator:id/op_sqrt")
+	private MobileElement btnSqrt;
+	@AndroidFindBy(id = "com.google.android.calculator:id/del")
+	private MobileElement btndel;
+	@AndroidFindBy(id = "com.google.android.calculator:id/clr")
+	private MobileElement btnClear;
+
+	public void simpleSum(Integer a, Integer b) {
+
+		selectNumber(a);
+		pressAdd();
+		selectNumber(b);
+		pressEquals();
+	}
+
+	private void selectNumber(Integer a) {
+		click(DriverFactory.getDriver().findElementByAccessibilityId(a.toString()));
+	}
+
+	private void pressAdd() {
+		click(getBtnAdd());
+	}
+
+	private void pressMinus() {
+		click(getBtnMinus());
+	}
+
+	private MobileElement getBtnMinus() {
+
+		return btnMinus;
+	}
+
+	private void pressTimes() {
+		click(getBtnTimes());
+	}
+
+	private MobileElement getBtnTimes() {
+
+		return btnTimes;
+	}
+
+	private void pressDividedTo() {
+		click(getBtnDividedTo());
+	}
+
+	private MobileElement getBtnDividedTo() {
+
+		return btnDividedTo;
+	}
+
+	private void pressSqrt() {
+		click(getBtnSqrt());
+	}
+
+	private MobileElement getBtnSqrt() {
+
+		return btnSqrt;
+	}
+
+	private MobileElement getBtnAdd() {
+		return btnAdd;
+	}
+
+	private MobileElement getBtnEquals() {
+		return btnEquals;
+	}
+
+	private MobileElement getLblResult() {
+		return lblResult;
+	}
+
+	private void pressEquals() {
+		click(getBtnEquals());
+	}
+
+	private MobileElement getBtnPercent() {
+		return btnPercent;
+	}
+
+	private MobileElement getBtndel() {
+		return btndel;
+	}
+
+	private MobileElement getBtnClear() {
+		return btnClear;
+	}
+
+	public String getResult() {
+		return getText(getLblResult());
+	}
+
 }
