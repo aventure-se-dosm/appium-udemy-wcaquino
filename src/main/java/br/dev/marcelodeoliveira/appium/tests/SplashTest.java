5d9d52e93a0e4d44e6ed1bb05f36ca99a2c79f3a
Marcelo de Oliveira Santos <=>
2023-09-15 17:42:40 -0300

    Starting lession 27 - Splash

---
+++ src/main/java/br/dev/marcelodeoliveira/appium/tests/SplashTest.java
@@ -0,0 +1,20 @@
+package br.dev.marcelodeoliveira.appium.tests;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SplashTest extends BaseTest {
+
+	@Ignore
+	@Test
+	public void deveAguardarSplashSumir() {
+		//acessar menu 'Splash';
+		
+		//verificar que o Splash é exibido;
+		
+		//aguardar a saída do Splash;
+		
+		//verificar que o formulário é exibido.
+		
+	}
+}
---
+++ src/main/java/br/dev/marcelodeoliveira/appium/tests/model/pages/SplashPage.java
@@ -0,0 +1,9 @@
+package br.dev.marcelodeoliveira.appium.tests.model.pages;
+
+import io.appium.java_client.MobileElement;
+
+public class SplashPage extends BasePage {
+
+	MobileElement splash;
+	MobileElement txtSplashBemVindo;
+}
