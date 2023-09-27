86c5e0975a47c4481d26b38c211bfc2f3d300725
Marcelo de Oliveira Santos <=>
2023-09-27 18:03:02 -0300

    Changing SwipeTestPage webelements gathering from annoted into dynamic

--- src/main/java/br/dev/marcelodeoliveira/appium/tests/model/pages/SwipeListPage.java
+++ src/main/java/br/dev/marcelodeoliveira/appium/tests/model/pages/SwipeListPage.java
@@ -4,59 +4,79 @@ import org.openqa.selenium.By;
 
 import io.appium.java_client.MobileElement;
 
-
-
 public class SwipeListPage extends BasePage {
-	
 
-	 enum Sinal {
-		POSITIVO("+"),
-		NEGATIVO("-");
-		
-		private String sinal;
-		private Sinal(String sinal) {
-			this.sinal = sinal;
+	enum Signal {
+		POSITIVO("+"), NEGATIVO("-");
+
+		private String signal;
+
+		private Signal(String signal) {
+			this.signal = signal;
 		};
-		
-		//como ocultar?
-		public String getSinal() {
-			return this.sinal;
+
+		// como ocultar?
+		public String getsignal() {
+			return this.signal;
 		}
-		public String getSinalBetweenBrackets() {
-			return String.format("(%s)", getSinal());
+
+		public String getsignalBetweenBrackets() {
+			return String.format("(%s)", getsignal());
+		}
+
+		public static Signal getValueOf(String signal) {
+			return Signal.valueOf(signal.toUpperCase());
 		}
 	}
+
 //	private String getOptByXpath(Integer index) {
 //		return getOptByXpath(index.toString());
 //	}
-	 private String getOptionBaseXpath() {
-		 return ("//android.widget.TextView[contains(@text, 'Opção %s')]");
-	 }
-	 private String getOptionAndSignalBaseXpath() {
-		 return ("//android.widget.TextView[contains(@text, 'Opção %s (%s)')]");
-	 }
-	 
-	 private String getSignalXpath() {
-		 return ("//android.widget.TextView[contains(@text, '(%s)')]");
-	 }
-	 
-	 
+	private String getOptionXpath(String index) {
+		return String.format("//android.widget.TextView[contains(@text, 'Opção %s')]", index);
+	}
+	private String getOptionSignalFrame(String index, String signal) {
+		return String.format("//android.widget.TextView[contains(@text, 'Opção %s')]/following-sibling::*[@text='%s']", index, signal);
+	}
+
+	private String getOptionAndSignalBaseXpath(String index) {
+		return ("//android.widget.TextView[contains(@text, 'Opção %s (%s)')]");
+	}
+
+	private String getSignalXpath() {
+		return ("//android.widget.TextView[contains(@text, '(%s)')]");
+	}
+
 	private MobileElement getOptElelmByXpath(String index) {
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return getElement(
-				By.xpath(String.format("//android.widget.TextView[contains(@text, 'Opção %s')]", index)));
+		return getElement(By.xpath(getOptionXpath(index)));
 	}
 
-	public void swipeEsquerda(String index, String sinal) {
+	public void swipeEsquerda(String index, String signal) {
 		swipeLeft(getOptElelmByXpath(index).getCenter());
-		 //click(element);
+		clickByIndexAndsignal(index, signal);
 	}
 
-	public void swipeDireita(String index, String sinal) {
+	private void clickByIndexAndsignal(String index, String signal) {
+		switch (Signal.getValueOf(signal)) {
+			case POSITIVO: {
+				click(getElement(getOptElelmByXpath(index)));
+				break;
+			}
+			case NEGATIVO: {
+				
+				break;
+			}
+		}
+		;
+
+	}
+
+	public void swipeDireita(String index, String signal) {
 		swipeRight(getOptElelmByXpath(index).getCenter());
 	}
 
