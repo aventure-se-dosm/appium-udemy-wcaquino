faf1b708b4f0b530969e712fcfc8b930a5f573de
Marcelo de Oliveira Santos <=>
2023-10-16 11:50:15 -0300

    setting up 'saucelabs testing'

--- src/main/java/br/dev/marcelodeoliveira/appium/core/DriverFactory.java
+++ src/main/java/br/dev/marcelodeoliveira/appium/core/DriverFactory.java
@@ -4,6 +4,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -12,9 +14,10 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class DriverFactory {
-	private static AndroidDriver<MobileElement> androidDriver;
+	private static AndroidDriver<MobileElement> androidDriver, testDriver;
 	private static WebDriver webDriver;
 	private static DesiredCapabilities desiredCapabilities;
+	private static MutableCapabilities caps, sauceOptions;
 
 	public void setupTest() {
 		setupDriver();
@@ -39,6 +42,33 @@ public class DriverFactory {
 		desiredCapabilities.setCapability("automationName", "uiautomator2");
 		desiredCapabilities.setCapability("fullReset", false);
 		desiredCapabilities.setCapability("noReset", true);
+	}
+
+	public static void setTestDriverCapabilities() {
+
+		setTestCapsCapabilites();
+
+		setSouceTestCapabilites();
+
+		caps.setCapability("sauce:options", sauceOptions);
+	}
+
+	public static void setTestCapsCapabilites() {
+		caps = new DesiredCapabilities();
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("appium:app", "storage:filename=CTAppium_2_0.apk"); // The filename of the mobile app
+		// caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+		// caps.setCapability("appium:platformVersion", "12.0");
+		caps.setCapability("appium:automationName", "UiAutomator2");
+	}
+
+	public static void setSouceTestCapabilites() {
+		sauceOptions = new DesiredCapabilities();
+		sauceOptions.setCapability("username", "oauth-markeepgoin-8443f");
+		sauceOptions.setCapability("accessKey", "9ba09152-a3ec-46b4-bf4b-96448a62a0ac");
+		sauceOptions.setCapability("build", "appium-build-T9BD9");
+		sauceOptions.setCapability("name", "<your test name>");
+		// sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
 	}
 
 	public static void setDefaultCapabilitiesCalc() {
@@ -73,6 +103,10 @@ public class DriverFactory {
 		return androidDriver == null;
 	}
 
+	private static boolean isTestDriverNull() {
+		return testDriver == null;
+	}
+
 	private static boolean isWebDriverNull() {
 		return webDriver == null;
 	}
@@ -83,6 +117,7 @@ public class DriverFactory {
 
 	public static void setupDriver() {
 		androidDriver = null;
+
 		try {
 			androidDriver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), getCapabilities());
 			androidDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -91,11 +126,41 @@ public class DriverFactory {
 		}
 	}
 
+	public static void setupTestDriver() {
+
+		androidDriver = null;
+		setTestDriverCapabilities();
+		try {
+			androidDriver = new AndroidDriver<MobileElement>(
+					new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub"), caps);
+			// androidDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Capabilities getTestCapsCapabilities() {
+		// TODO Auto-generated method stub
+		return caps;
+	}
+
+	public static Capabilities getTestSauceOptionCapabilities() {
+		// TODO Auto-generated method stub
+		return sauceOptions;
+	}
+
 	public static AndroidDriver<MobileElement> getDriver() {
 		if (isDriverNull()) {
 			setupDriver();
 		}
 		return androidDriver;
+	}
+
+	public static AndroidDriver<MobileElement> getTestDriver() {
+		if (isTestDriverNull()) {
+			setupTestDriver();
+		}
+		return testDriver;
 	}
 
 	public static AndroidDriver<MobileElement> getWebDriver() {
@@ -108,14 +173,24 @@ public class DriverFactory {
 	public static void killDriver() {
 		if (!isDriverNull())
 			androidDriver.quit();
-		setDriverNull();
+		setDriverNull(getDriver());
 
 	}
 
 	public static void killWebDriver() {
 		if (!isWebDriverNull())
 			webDriver.quit();
-		setWebDriverNull();
+		setDriverNull(getWebDriver());
+	}
+
+	public static void killTestDriver() {
+		if (!isWebDriverNull())
+			webDriver.quit();
+		setDriverNull(getTestDriver());
+	}
+
+	public static void setDriverNull(WebDriver driver) {
+		driver = null;
 	}
 
 	private static void setDriverNull() {
